dc4ba745786008a498fbfc382d00129eb4eddafd
SoniaZabara <127392732+SoniaZabara@users.noreply.github.com>
2024-11-19 19:41:12 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -1,22 +1,23 @@
 import socket
 import time
 
+
 class ClientUDP:
-    def __init__(self,udp_ip="127.0.0.1",udp_port=5005,peer_ip="127.0.0.1",peer_port=5004,buffer_size=1024,format='utf-8'):
-        self.udp_address=(udp_ip,udp_port)
-        self.peer_address=(peer_ip,peer_port)
+    def __init__(self, udp_ip="127.0.0.1", udp_port=5005, peer_ip="127.0.0.1", peer_port=5004, buffer_size=1024, format='utf-8'):
+        self.udp_address = (udp_ip, udp_port)
+        self.peer_address = (peer_ip, peer_port)
         self.buffer_size = buffer_size
         self.format = format
-        self.disconnect_message="Disconnect please !"
+        self.disconnect_message = "Disconnect please !"
         self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.client.settimeout(1)
         self.client.bind(self.udp_address)
 
-    def send_message(self,message):
+    def send_message(self, message):
         try:
             print(f"Client -> Sending message: {message}")
-            self.client.sendto(message.encode(self.format),self.peer_address)
+            self.client.sendto(message.encode(self.format), self.peer_address)
 
             data_bytes, address = self.client.recvfrom(self.buffer_size)
             msg = data_bytes.decode(self.format)
@@ -35,5 +36,3 @@ class ClientUDP:
         self.send_message("Hello Server")
         time.sleep(1) # to give to time to server to process the message
         self.disconnect()
-
-#client
--- server.py
+++ server.py
@@ -2,20 +2,20 @@ import socket
 import threading
 import time
 
+
 class ServerUDP:
-    def __init__(self,udp_ip="127.0.0.1",udp_port=5004,peer_ip="127.0.0.1",peer_port=5005,buffer_size=1024,format='utf-8'):
-        self.udp_address=(udp_ip,udp_port)
-        self.peer_address=(peer_ip,peer_port)
-        self.buffer_size=buffer_size
-        self.format=format
-        self.disconnect_message="Server -> Disconnect please !"
-        self.message_send="Server -> Hello Client !"
+    def __init__(self, udp_ip="127.0.0.1", udp_port=5004, peer_ip="127.0.0.1", peer_port=5005, buffer_size=1024, format='utf-8'):
+        self.udp_address = (udp_ip, udp_port)
+        self.peer_address = (peer_ip, peer_port)
+        self.buffer_size = buffer_size
+        self.format = format
+        self.disconnect_message = "Disconnect please !"
+        self.message_send = "Server -> Hello Client !"
 
         self.server = socket.socket(family=socket.AF_INET,  # Address family InterNET IP4
                                     type=socket.SOCK_DGRAM)  # UDP type of socket
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind(self.udp_address)  # when receiving, verify the source IP of the packet
-
 
     def handle_client(self):
         print("Server -> Ready!")
@@ -30,16 +30,16 @@ class ServerUDP:
 
                     if msg == self.disconnect_message:
                         print("Server -> Disconnect message received. Server disconnected.")
+                        connected = False
 
                     else:
                         # send response to client
                         self.server.sendto(self.message_send.encode(self.format), self.peer_address)
-                        self.server.close()
                         # sendto instead of send because is UDP it needs the extra information
 
             except Exception as e:
                 print(f"Server -> Error : {e}")
-                connected=False
+                connected = False
                 break
 
         self.server.close()
@@ -48,5 +48,4 @@ class ServerUDP:
     def run(self):
         thread = threading.Thread(target=self.handle_client, args=())
         thread.start()
-        print(f"Server -> Starting.. Active threads :{threading.active_count() - 1}") # how many threads
-#server
+        print(f"Server -> Starting.. Active threads :{threading.active_count() - 1}")  # how many threads
