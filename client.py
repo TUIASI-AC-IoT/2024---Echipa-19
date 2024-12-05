15294d02ef274d3fbfe5ca457b2a327678e1aa37
BRAN IOANA ANDREEA <ioana-andreea.bran@student.tuiasi.ro>
2024-12-05 14:30:13 +0200

    actualizare client si server cu functii separate pentru send si receive

--- client.py
+++ client.py
@@ -1,39 +1,70 @@
+
 import socket
 import time
+import threading
 
 class ClientUDP:
-    def __init__(self,udp_ip="127.0.0.1",udp_port=5005,peer_ip="127.0.0.1",peer_port=5004,buffer_size=1024,format='utf-8'):
-        self.udp_address=(udp_ip,udp_port)
-        self.peer_address=(peer_ip,peer_port)
+    def __init__(self, udp_ip="127.0.0.1", udp_port=5005, peer_ip="127.0.0.1", peer_port=5004, buffer_size=1024, format='utf-8'):
+        self.udp_address = (udp_ip, udp_port)
+        self.peer_address = (peer_ip, peer_port)
         self.buffer_size = buffer_size
         self.format = format
-        self.disconnect_message="Disconnect please !"
+        self.disconnect_message = "Disconnect please !"
         self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.client.settimeout(1)
         self.client.bind(self.udp_address)
 
-    def send_message(self,message):
-        try:
-            print(f"Client -> Sending message: {message}")
-            self.client.sendto(message.encode(self.format),self.peer_address)
+        self.client.settimeout(5)
 
-            data_bytes, address = self.client.recvfrom(self.buffer_size)
-            msg = data_bytes.decode(self.format)
-            print(f"Client -> Message received : {msg}")
+        self.connected=True #flag pt a controla oprirea thread-urilor
+
+    def send_message(self):
+        try:
+            while self.connected:
+
+                message=input("Client -> Enter message to send: ")
+                self.client.sendto(message.encode(self.format), self.peer_address)
+
+                if message == self.disconnect_message:
+                    self.connected = False
 
         except socket.timeout:
             print("Client -> TIMEOUT: No response from server")
         except ConnectionResetError:
             print("Client -> Connection was closed by the server")
 
+    def receive_message(self):
+        try:
+            while self.connected:
+                try:
+                    data_bytes, address = self.client.recvfrom(self.buffer_size)
+                    msg = data_bytes.decode(self.format)
+                    print(f"Server -> {msg}")
+                    if msg=="Server -> Goodbye!":
+                        print("Client -> Server disconnected")
+                        self.connected = False
+                except socket.timeout:
+                    continue
+        except Exception as e:
+            print(f"Client -> Error in receiving message {e}")
+
     def disconnect(self):
+        self.connected=False
         self.send_message(self.disconnect_message)
         self.client.close()
+        print("Client -> Disconnected. ")
 
     def run(self):
-        self.send_message("Hello Server")
-        time.sleep(1) # to give to time to server to process the message
-        self.disconnect()
+        try:
+            send_thread=threading.Thread(target=self.send_message)
+            recv_thread=threading.Thread(target=self.receive_message)
 
-#client
+            send_thread.start()
+            recv_thread.start()
+            print(self.udp_address)
+            send_thread.join()
+            recv_thread.join()
+
+        except Exception as e:
+            print(f"Client -> Error in running thread {e}")
--- server.py
+++ server.py
@@ -2,51 +2,79 @@ import socket
 import threading
 import time
 
+
 class ServerUDP:
-    def __init__(self,udp_ip="127.0.0.1",udp_port=5004,peer_ip="127.0.0.1",peer_port=5005,buffer_size=1024,format='utf-8'):
-        self.udp_address=(udp_ip,udp_port)
-        self.peer_address=(peer_ip,peer_port)
-        self.buffer_size=buffer_size
-        self.format=format
-        self.disconnect_message="Server -> Disconnect please !"
-        self.message_send="Server -> Hello Client !"
+    def __init__(self, udp_ip="127.0.0.1", udp_port=5004, peer_ip="127.0.0.1", peer_port=5005, buffer_size=1024, format='utf-8'):
+        self.udp_address = (udp_ip, udp_port)
+        self.peer_address = (peer_ip, peer_port)
+        self.buffer_size = buffer_size
+        self.format = format
+        self.disconnect_message = "Disconnect please !"
+        self.message_send = "Hello Client !"
 
         self.server = socket.socket(family=socket.AF_INET,  # Address family InterNET IP4
                                     type=socket.SOCK_DGRAM)  # UDP type of socket
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind(self.udp_address)  # when receiving, verify the source IP of the packet
 
+        self.connected = True
 
     def handle_client(self):
         print("Server -> Ready!")
-        connected = True
-        while connected:
+        while self.connected:
             try:
                 data_bytes, client_address = self.server.recvfrom(self.buffer_size)
                 msg = data_bytes.decode(self.format)
 
-                if data_bytes:
+                if msg:
                     print(f"Server -> Message received : {msg} from : {client_address}")
 
                     if msg == self.disconnect_message:
                         print("Server -> Disconnect message received. Server disconnected.")
-
+                        self.connected = False
+                        self.server.sendto("Server -> Goodbye!".encode(self.format),client_address)
+                        break
                     else:
-                        # send response to client
-                        self.server.sendto(self.message_send.encode(self.format), self.peer_address)
-                        self.server.close()
-                        # sendto instead of send because is UDP it needs the extra information
+                        response="Server -> Received: {msg}"
+                        self.server.sendto(response.encode(self.format), self.peer_address)
 
             except Exception as e:
                 print(f"Server -> Error : {e}")
-                connected=False
+                self.connected = False
                 break
 
-        self.server.close()
-        print("Server -> Server disconnected.")
+    def send_message(self):
+        while self.connected:
+            try:
+                # send response to client
+                self.server.sendto(self.message_send.encode(self.format), self.peer_address)
+                # sendto instead of send because is UDP it needs the extra information
+                print(f"Server -> Message sent : {self.message_send} from : {self.peer_address}")
+                time.sleep(2)
+            except socket.timeout:
+                continue
+            except Exception as e:
+                print(f"Server -> Error in sending : {e}")
+                self.connected = False
+                break
 
     def run(self):
-        thread = threading.Thread(target=self.handle_client, args=())
-        thread.start()
-        print(f"Server -> Starting.. Active threads :{threading.active_count() - 1}") # how many threads
-#server
+        try:
+            recv_thread = threading.Thread(target=self.handle_client, args=())
+            send_thread = threading.Thread(target=self.send_message, args=())
+
+            recv_thread.start()
+            send_thread.start()
+
+            print(f"Server -> Starting.. Active threads :{threading.active_count() - 1}")  # how many threads
+            print(self.udp_address)
+
+            recv_thread.join()
+            send_thread.join()
+
+        except Exception as e:
+            print(f"Server -> Error : {e}")
+        finally:
+            self.connected = False
+            self.server.close()
+            print("Server closed")
